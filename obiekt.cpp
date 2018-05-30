505a9e28974d2fc4d155910f8711184673e069ee
Your Name <michal9797@gmail.com>
2018-05-30 14:24:58 +0200

    Działa z klasasmi, dodane dziedziczenie i naprawiony projekt codeblocks

---
+++ obiekt.cpp
@@ -0,0 +1,87 @@
+#include "obiekt.hpp"
+
+Obiekt::Obiekt(GLuint* v)
+{
+	vao = v;
+}
+
+Obiekt::~Obiekt()
+{
+	Destroy();
+}
+
+
+void Obiekt::Destroy()
+{
+		//Delete VBOs
+	glDeleteBuffers(1,&bufVertices);
+	glDeleteBuffers(1,&bufColors);
+	glDeleteBuffers(1,&bufNormals);
+}
+
+
+
+
+//Creates a VBO buffer from an array
+GLuint Obiekt::makeBuffer(void *data, int vertexCount, int vertexSize) {
+	GLuint handle;
+
+	glGenBuffers(1,&handle);//Generate handle for VBO buffer
+	glBindBuffer(GL_ARRAY_BUFFER,handle);  //Active the handle
+	glBufferData(GL_ARRAY_BUFFER, vertexCount*vertexSize, data, GL_STATIC_DRAW);//Copy the array to VBO
+
+	return handle;
+}
+
+//Assigns VBO buffer handle to an attribute of a given name
+void Obiekt::assignVBOtoAttribute(ShaderProgram *shaderProgram,const char* attributeName, GLuint bufVBO, int vertexSize) {
+	GLuint location=shaderProgram->getAttribLocation(attributeName); //Get slot number for the attribute
+	glBindBuffer(GL_ARRAY_BUFFER,bufVBO);  //Activate VBO handle
+	glEnableVertexAttribArray(location); //Turn on using of an attribute of a number passed as an argument
+	glVertexAttribPointer(location,vertexSize,GL_FLOAT, GL_FALSE, 0, NULL); //Data for the slot should be taken from the current VBO buffer
+}
+
+
+
+
+//Preparation for drawing of a single object
+void Obiekt::prepareObject(ShaderProgram* shaderProgram) {
+
+
+
+	//Build VBO buffers with object data
+	bufVertices=makeBuffer(&vertices[0], vertices.size(), sizeof(glm::vec3)); //VBO with vertex coordinates
+	bufUVs=makeBuffer(&uvs[0], uvs.size(), sizeof(glm::vec2)); //VBO with texture coordiantes
+	//bufColors=makeBuffer(&uvs, uvs.size() , sizeof(glm::vec2));//VBO with vertes colors
+	bufNormals=makeBuffer(&normals[0], normals.size(), sizeof(glm::vec3));//VBO with vertex normals
+
+	//Create VAO which associates VBO with attributes in shading program
+	glGenVertexArrays(1,vao); //Generate VAO handle and store it in the global variable
+
+	glBindVertexArray(*vao); //Activate newly created VAO
+
+	assignVBOtoAttribute(shaderProgram,"vertex",bufVertices,3); //"vertex" refers to the declaration "in vec4 vertex;" in vertex shader
+	assignVBOtoAttribute(shaderProgram,"vertexUV",bufUVs,2); //"UV" refers to the declaration "in vec4 UV;" in vertex shader
+	assignVBOtoAttribute(shaderProgram,"normal",bufNormals,3); //"normal" refers to the declaration "in vec4 normal;" w vertex shader
+
+	bindTextures();
+
+	glBindVertexArray(0); //Deactivate VAO
+}
+
+
+
+void Obiekt::drawObject(GLuint fvao, ShaderProgram *shaderProgram, mat4 mP, mat4 mV, mat4 mM) {
+	glUniformMatrix4fv(shaderProgram->getUniformLocation("P"),1, false, glm::value_ptr(mP));
+	glUniformMatrix4fv(shaderProgram->getUniformLocation("V"),1, false, glm::value_ptr(mV));
+	glUniformMatrix4fv(shaderProgram->getUniformLocation("M"),1, false, glm::value_ptr(mM));
+
+	//Activation of VAO and therefore making all associations of VBOs and attributes current
+	glBindVertexArray(fvao);
+
+	//Drawing of an object
+	glDrawArrays(GL_TRIANGLES,0,vertices.size());
+
+	//Tidying up after ourselves (not needed if we use VAO for every object)
+	glBindVertexArray(0);
+}
---
+++ obiekt.hpp
@@ -0,0 +1,41 @@
+#ifndef OBIEKT_H
+#define OBIEKT_H
+
+
+#include "init.hpp"
+class Obiekt
+{
+public:
+	explicit Obiekt(GLuint*);
+	virtual ~Obiekt();
+
+	
+	GLuint makeBuffer(void*, int , int );
+	void assignVBOtoAttribute(ShaderProgram*,const char*, GLuint, int);
+	void prepareObject(ShaderProgram*);
+	void drawObject(GLuint, ShaderProgram*, mat4, mat4, mat4);
+	void Destroy();
+	
+	virtual void bindTextures(){}
+	virtual void load_stuff(ShaderProgram*){}
+
+////////////////////////////////////
+	GLuint* vao;
+	GLuint Texture;
+	GLuint Texture_cr;
+	GLuint TextureID;
+	GLuint TextureID_cr;
+	GLuint bufVertices; //handle for VBO buffer which stores vertex coordinates
+	GLuint bufUVs;
+	GLuint bufColors;  //handle for VBO buffer which stores vertex colors
+	GLuint bufNormals; //handle for VBO buffer which stores vertex normals
+
+	 // Read our .obj file
+ 	std::vector<glm::vec3> vertices;
+ 	std::vector<glm::vec2> uvs;
+ 	std::vector<glm::vec3> normals; 
+
+};
+
+
+#endif
